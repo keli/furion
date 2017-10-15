0932a8ee80bc408a39cc8aec25bd7fa0188d45e5
Keli Hu <dev@keli.hu>
2017-10-15 20:44:51 +0800

    pump version to 1.4.7

--- scripts/pyinstaller/pyinstaller.bat
+++ scripts/pyinstaller/pyinstaller.bat
@@ -1,2 +1,2 @@
-python -O C:\pyinstaller\pyinstaller.py --onefile --icon=furion.ico ..\furion.py
+python -O C:\pyinstaller\pyinstaller.py --onefile --icon=furion.ico ..\..\furion\furion.py
 python -O C:\pyinstaller\pyinstaller.py furion.spec
--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='1.4.6',
+    version='1.4.7',
 
     description='A socks5 proxy with ssl support',
     long_description=readme,
